fe039beb5b2ca47989af2423ba0c53430a0a949c
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-23 21:52:01 +0100

    update input styles

--- src/components/secondary-components/Input.js
+++ src/components/secondary-components/Input.js
@@ -1,28 +1,30 @@
 //importar las propTypes
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 //styles
-import '../../styles/components/input.scss';
+import "../../styles/components/input.scss";
 
 const Input = (props) => {
-   //handle
+  //handle
   const handleChange = (ev) => {
     props.handleChange(ev.target.value);
   };
-  
+
   return (
-    <label className="label" htmlFor={props.id}>
-      {props.labelText}
-      <input
-        className="form-control me-2"
-        id={props.id}
-        type={props.inputType}
-        name={props.inputName}
-        placeholder={props.inputPlaceholder}
-        value={props.inputValue}
-        onChange={handleChange}
-      />
-      <i className="fas fa-search"></i>
-    </label>
+    <>
+      <label className="inner-addon left-addon" htmlFor={props.id}>
+        {props.labelText}
+        <i className="fas fa-search"></i>
+        <input
+          className="form-control me-2"
+          id={props.id}
+          type={props.inputType}
+          name={props.inputName}
+          placeholder={props.inputPlaceholder}
+          value={props.inputValue}
+          onChange={handleChange}
+        />
+      </label>
+    </>
   );
 };
 // DefalutProps
@@ -32,7 +34,7 @@ Input.defaultProps = {
   name: "search",
   inputType: "text",
   placeholder: "Escribe aqui...",
-  labelText: "Escribe aqui: "
+  labelText: "Escribe aqui: ",
 };
 
 // PropTypes
