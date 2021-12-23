620954bce8147b2a1d668a40bc07b470dcea559e
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-23 11:44:52 +0100

    update structure& styles

--- src/components/LateralMenu.js
+++ src/components/LateralMenu.js
@@ -1,52 +1,74 @@
 import { NavLink } from 'react-router-dom';
+import logo from "../images/logo4.png";
 //Styles
 import '../styles/layout/lateralMenu.scss';
 
 const LateralMenu = (props) => {
   return (
     <nav>
-      <ul>
-        <li>
-           <img src="../images/logo.png" alt='logo'/>
+      <ul className="nav flex-column">
+        <li className="first-element">
+          <img className='logo' src={logo} alt="logo" />
         </li>
+
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-          <button className="btn btn-line"><i className="fas fa-home"></i></button>
+            <button className="btn btn-line">
+              <i className="fas fa-home"></i>
+            </button>
           </NavLink>
         </li>
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-          <button className="btn btn-line"><i className="far fa-check-square"></i></button>
+            <button className="btn btn-line">
+              <i className="far fa-check-square"></i>
+            </button>
           </NavLink>
         </li>
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-          <button className="btn btn-line"><i className="far fa-envelope"></i></button>
+            <button className="btn btn-line">
+              <i className="far fa-envelope"></i>
+            </button>
           </NavLink>
         </li>
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-          <button className="btn btn-line"><i className="far fa-user"></i></button>
+            <button className="btn btn-line">
+              <i className="far fa-user"></i>
+            </button>
           </NavLink>
         </li>
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-         <button className="btn btn-line"> <i className="far fa-calendar-alt"></i></button>
+            <button className="btn btn-line">
+              {" "}
+              <i className="far fa-calendar-alt"></i>
+            </button>
           </NavLink>
         </li>
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-         <button className="btn btn-line"> <i className="far fa-folder-open"></i></button>
+            <button className="btn btn-line">
+              {" "}
+              <i className="far fa-folder-open"></i>
+            </button>
           </NavLink>
         </li>
         <li>
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-         <button className="btn btn-line"> <i className="far fa-star"></i></button>
+            <button className="btn btn-line">
+              {" "}
+              <i className="far fa-star"></i>
+            </button>
           </NavLink>
         </li>
-        <li>
+        <li className="last-element">
           <NavLink to="/" title="inicio" activeClassName="selected-link">
-         <button className="btn btn-line"> <i className="fas fa-cog"></i></button>
+            <button className="btn btn-line">
+              {" "}
+              <i className="fas fa-cog"></i>
+            </button>
           </NavLink>
         </li>
       </ul>
