fb7c170a532e32f56b2cf5ff4dc41e66a616b712
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-27 15:17:16 +0100

    update test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../components/Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../components/Header";
 
 test('renders "Proyecto"', () => {
   // Arrange
   render(<Header />);
   // Act
-  const divElement = screen.getByText("Proyecto");
+  const divElement = screen.getByText("Planning/ Nombre del Proyecto");
+  // console.log(`<<<<<<<<<<<<`, divElement);
   // Assert
   expect(divElement).toBeInTheDocument();
 });
